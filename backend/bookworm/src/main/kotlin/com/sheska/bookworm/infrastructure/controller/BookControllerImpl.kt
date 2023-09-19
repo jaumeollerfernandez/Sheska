b5b3502df0cedc86c6b90dc5781c0dc1d8b66f79
JaumeKothan <jaumeollerfernandez@gmail.com>
2023-09-19 20:50:10 +0200

    docker files added, must do proper connection with api/db

--- backend/bookworm/src/main/kotlin/com/sheska/bookworm/infrastructure/controller/BookControllerImpl.kt
+++ backend/bookworm/src/main/kotlin/com/sheska/bookworm/infrastructure/controller/BookControllerImpl.kt
@@ -5,35 +5,38 @@ import com.sheska.bookworm.application.dto.BookResponse
 import com.sheska.bookworm.application.dto.UpdateBookRequest
 import com.sheska.bookworm.application.service.BookManagementService
 import com.sheska.bookworm.infrastructure.controller.BookControllerImpl.Companion.BASE_BOOK_URL
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.net.URI
 
 @RestController
 @RequestMapping(value = [BASE_BOOK_URL])
 class BookControllerImpl(private val bookManagementService: BookManagementService) : BookController {
-    @GetMapping("/{id}")
+    @GetMapping("/{isbn}")
     override fun findByISBN(@PathVariable isbn: Long): ResponseEntity<BookResponse>? {
-        val bookResponse = this.bookManagementService.findByISBN(isbn)
-
+        val bookResponse: BookResponse? = this.bookManagementService.findByISBN(isbn)
+        return ResponseEntity.status(HttpStatus.OK).body(bookResponse)
     }
-
+    @GetMapping("")
     override fun findAll(): ResponseEntity<List<BookResponse>> {
-        TODO("Not yet implemented")
+        val listOfBooks = this.bookManagementService.findAll()
+        return ResponseEntity.status(HttpStatus.OK).body(listOfBooks)
+    }
+    @PostMapping
+    override fun save(@RequestBody addBookRequest: AddBookRequest): ResponseEntity<BookResponse> {
+        val bookResponse = this.bookManagementService.save(addBookRequest)
+        return ResponseEntity.created(URI.create(BASE_BOOK_URL.plus("/${bookResponse.isbn}"))).body(bookResponse)
+    }
+    @PutMapping
+    override fun update(@RequestBody updateBookRequest: UpdateBookRequest): ResponseEntity<BookResponse> {
+        return ResponseEntity.ok(this.bookManagementService.update(updateBookRequest))
     }
 
-    override fun save(addBookRequest: AddBookRequest): ResponseEntity<BookResponse> {
-        TODO("Not yet implemented")
-    }
-
-    override fun update(updateBookRequest: UpdateBookRequest): ResponseEntity<BookResponse> {
-        TODO("Not yet implemented")
-    }
-
-    override fun deleteByISBN(isbn: Long): ResponseEntity<Unit> {
-        TODO("Not yet implemented")
+    @DeleteMapping("{isbn}")
+    override fun deleteByISBN(@PathVariable isbn: Long): ResponseEntity<Unit> {
+        this.bookManagementService.deleteByISBN(isbn)
+        return ResponseEntity.noContent().build()
     }
 
     companion object{
